40c2915405e84d2715b846075a05a26be96ebb2c
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2022-10-24 11:08:49 +0300

    feat:добавила слайдер в попап

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,24 +4,41 @@ const galleryPopup = document.querySelector('.popup')
 
 
 function openPopup(popup) {
+  splide2.mount();
   popup.classList.add('popup_opened');
 }
 
 function popupClose(popup) {
+  splide2.destroy();
   popup.classList.remove('popup_opened');
 }
+
 
 const gallery = document.querySelector('.gallery');
 const galleryPhoto = gallery.querySelectorAll('.gallery-photo__item');
 const picture = document.querySelector('.popup__picture');
+const photoContainer = document.querySelector('.popup__photo-container')
+const photosTemplate = document.querySelector('.photos').content;
+
+fillPopup();
 
 galleryPhoto.forEach( item => {
   item.addEventListener('click', () => {
-    picture.src = item.src;
     openPopup(galleryPopup);
   })
 })
 
+function fillPopup() {
+  const photos = Array.from(galleryPhoto);
+  photos.forEach( item => {
+    const imgPopup = photosTemplate.cloneNode(true);
+    imgPopup.querySelector('.photo').src = item.src;
+    photoContainer.append(imgPopup);
+  });
+}
+
+
+console.log()
   closePopup.addEventListener('click', evt => {
     popupClose(galleryPopup);
   });
--- scripts/slider.js
+++ scripts/slider.js
@@ -1,4 +1,4 @@
-var splide = new Splide( '#splide_place_perfomances', {
+var splide1 = new Splide( '#splide_place_perfomances', {
   perPage: 3,
   focus: 0,
   autoWidth: true,
@@ -11,6 +11,15 @@ var splide = new Splide( '#splide_place_perfomances', {
     },
   }
 } );
+splide1.mount();
 
+var splide2 = new Splide( '#splide2', {
+  perPage: 1,
+  pagination: false,
+  rewind: true,
+  rewindByDrag: true,
+  gap: '20px'
+  // width: '75vw'
+  // fixedWidth : '75vw',
+} );
 
-splide.mount();
